d298aef002f37d1592b7313571e7d7a11d197e9a
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-06 00:39:30 +0800

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -5,8 +5,8 @@ st.sidebar.header('選擇球隊及數據')
 option = st.sidebar.selectbox( '選擇分組？', ['Atlantic','Central', 'Southeast', 'Northwest','Pacific','Southwest'])
 if option=='Pacific':
   teams=st.sidebar.selectbox( '選擇球隊？', ['Golden State Warriors', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Phoenix Suns','Sacramento Kings'])
-  if teams=='Boston Celtics':
-    Pacific.BostonCeltics()
+  if teams=='Golden State Warriors':
+    Pacific.Golden_State_Warriors()
   if teams=='Brooklyn Nets':
     Pacific.BrooklynNets()
     
