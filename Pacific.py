e7c35b6c516694b7da6c74d0d7fd3a39c998f231
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-06 01:07:51 +0800

    Update Pacific.py

--- Pacific.py
+++ Pacific.py
@@ -1,5 +1,6 @@
 import streamlit as st  
-from PIL import Image  
+from PIL import Image
+
 def Golden_State_Warriors():
   col1, col2 = st.columns(2)
   with col1:
@@ -14,7 +15,8 @@ def Golden_State_Warriors():
   st.write('1971-1972年賽季開始，勇士隊改名為金州勇士（Golden State Warriors)。球隊離開舊金山，在奧克蘭進行絕大部份的主場比賽，沒有任何一場主場比賽於舊金山或戴利市舉行。但在那個球季中，有六場比賽的主場在於聖地牙哥舉行。')
   col1, col2= st.columns(2)
   col1.metric("聯盟冠軍🏆", "12  次")
-  col2.metric("分組冠軍🏆", "8  次")   
+  col2.metric("分組冠軍🏆", "8  次")  
+  
 def Los_Angeles_Clippers():
   col1, col2 = st.columns(2)
   with col1:
@@ -30,4 +32,19 @@ def Los_Angeles_Clippers():
   col1, col2= st.columns(2)
   col1.metric("聯盟冠軍🏆", "0  次")
   col2.metric("分組冠軍🏆", "2  次")
-
+  
+def Los_Angeles_Lakers():
+  col1, col2 = st.columns(2)
+  with col1:
+    image = Image.open('teams logo/Los_Angeles_Clippers_(2015).svg.png')
+    st.image(image) 
+  with col2:
+    st.title('Los Angeles Clippers')
+    st.subheader('老闆:Gillian Zucker')
+    st.subheader('GM:Michael Winger')
+    st.subheader('總教練:Tyronn Jamar Lue') 
+  st.write('水牛城勇士（1970年–1978年）-聖地牙哥快艇（1978年–1984年）-洛杉磯快艇（1984年至今）')
+  st.write('洛杉磯快艇隊成立於1970年，當時名為水牛城勇士。')
+  col1, col2= st.columns(2)
+  col1.metric("聯盟冠軍🏆", "0  次")
+  col2.metric("分組冠軍🏆", "2  次")
