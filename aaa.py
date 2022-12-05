d32ce3bdac806dc5c6e71ca13bdb4a8195997f44
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-06 00:32:42 +0800

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -1,12 +1,12 @@
 import streamlit as st  
-import ATLANTIC 
+import Pacific 
 st.title('NBA數據查詢系統')
 st.sidebar.header('選擇球隊及數據')
-option = st.sidebar.selectbox( '選擇分組？', ['ATLANTIC', '中央組', '東南組', '西北組','太平洋組','西南組'])
-if option=='ATLANTIC':
-  teams=st.sidebar.selectbox( '選擇球隊？', ['Boston Celtics', 'Brooklyn Nets', 'New York Knicks', 'Philadelphia 76ers','Toronto Raptors'])
+option = st.sidebar.selectbox( '選擇分組？', ['Atlantic','Central', 'Southeast', 'Northwest','Pacific','Southwest'])
+if option=='Pacific':
+  teams=st.sidebar.selectbox( '選擇球隊？', ['Golden State Warriors', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Phoenix Suns','Sacramento Kings'])
   if teams=='Boston Celtics':
-    ATLANTIC.BostonCeltics()
+    Pacific.BostonCeltics()
   if teams=='Brooklyn Nets':
-    ATLANTIC.BrooklynNets()
+    Pacific.BrooklynNets()
     
