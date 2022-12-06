00fff3ddcb0c2ae57e80896fb0eecbbce07b46f3
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-06 10:26:30 +0800

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -4,7 +4,9 @@ import Southwest
 st.title('NBA數據查詢系統')
 st.sidebar.header('選擇球隊及數據')
 option = st.sidebar.selectbox( '選擇分組？', ['Atlantic','Central', 'Southeast', 'Northwest','Pacific','Southwest'])
+
 if option=='Pacific':
+  
   teams=st.sidebar.selectbox( '選擇球隊？', ['Golden State Warriors', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Phoenix Suns','Sacramento Kings'])
   if teams=='Golden State Warriors':
     Pacific.Golden_State_Warriors()
@@ -16,15 +18,17 @@ if option=='Pacific':
     Pacific.Phoenix_Suns()
   if teams=='Sacramento Kings':
     Pacific.Sacramento_Kings() 
+    
 if option=='Southwest':
+  
   teams=st.sidebar.selectbox( '選擇球隊？', ['Dallas Mavericks', 'Houston Rockets', 'Memphis Grizzlies', 'New Orleans Pelicans','San Antonio Spurs'])
   if teams=='Dallas Mavericks':
-    Pacific.Dallas_Mavericks()
+    Southwest.Dallas_Mavericks()
   if teams=='Houston Rockets':
-    Pacific.Houston_Rockets()
+    Southwest.Houston_Rockets()
   if teams=='Memphis Grizzlies':
-    Pacific.Memphis_Grizzlies()
+    Southwest.Memphis_Grizzlies()
   if teams=='New Orleans Pelicans':
-    Pacific.New_Orleans_Pelicans()
+    Southwest.New_Orleans_Pelicans()
   if teams=='San Antonio Spurs':
-    Pacific.San_Antonio_Spurs()
+    Southwest.San_Antonio_Spurs()
