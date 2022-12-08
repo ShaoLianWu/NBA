66a5d0ee45cd72fd0e26ad072f02242e080905e1
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-08 09:12:50 +0800

    Update Pacific_map.py

--- Pacific_map.py
+++ Pacific_map.py
@@ -37,19 +37,19 @@ def Los_Angeles_Clippers_map():  #快艇
         st.image(image1)
     st.write('地址：1111 S Figueroa St, Los Angeles, CA 90015美國, 觀眾席數：19,060人')
     
-def Los_Angeles_Clippers_map():  #快艇
-    st.header('主場:加密貨幣網體育館')
-    Staples_Center= folium.Map(location=[34.043056, -118.267222], zoom_start=16)
+def Phoenix_Suns_map():  #太陽
+    st.header('主場:足跡中心')
+    Footprint_Center= folium.Map(location=[33.445833,-112.071389], zoom_start=16)
         # add marker for Liberty Bell
-    tooltip = "加密貨幣網體育館"
-    folium.Marker([34.043056, -118.267222], popup="加密貨幣網體育館", tooltip=tooltip
-    ).add_to(Staples_Center)
-    folium_static(Staples_Center)
+    tooltip = "足跡中心"
+    folium.Marker([33.445833,-112.071389], popup="足跡中心", tooltip=tooltip
+    ).add_to(Footprint_Center)
+    folium_static(Footprint_Center)
     col1, col2 = st.columns(2)
     with col1:
-        image = Image.open('teams picture/Staples_Center.jpg')
+        image = Image.open('teams picture/Footprint_Center.jpg')
         st.image(image)        
     with col2:        
-        image1 = Image.open('teams picture/Staples_Center_Tickets.jpg')
+        image1 = Image.open('teams picture/Footprint_Center_Tickets.jpg')
         st.image(image1)
-    st.write('地址：1111 S Figueroa St, Los Angeles, CA 90015美國, 觀眾席數：19,060人')  
+    st.write('地址：US Airways Center, 201S 1st St, Phoenix, AZ 85004美國, 觀眾席數：18,422人')  
