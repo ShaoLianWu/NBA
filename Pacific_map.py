fccb6fdfb16d4a890a4c003f9d350712f8697fdc
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-08 09:24:17 +0800

    Update Pacific_map.py

--- Pacific_map.py
+++ Pacific_map.py
@@ -37,6 +37,23 @@ def Los_Angeles_Clippers_map():  #快艇
         st.image(image1)
     st.write('地址：1111 S Figueroa St, Los Angeles, CA 90015美國, 觀眾席數：19,060人')
     
+def Los_Angeles_Lakers_map():  #湖人
+    st.header('主場:加密貨幣網體育館')
+    Staples_Center= folium.Map(location=[34.043056, -118.267222], zoom_start=16)
+        # add marker for Liberty Bell
+    tooltip = "加密貨幣網體育館"
+    folium.Marker([34.043056, -118.267222], popup="加密貨幣網體育館", tooltip=tooltip
+    ).add_to(Staples_Center)
+    folium_static(Staples_Center)
+    col1, col2 = st.columns(2)
+    with col1:
+        image = Image.open('teams picture/Staples_Center.jpg')
+        st.image(image)        
+    with col2:        
+        image1 = Image.open('teams picture/Staples_Center_Tickets.jpg')
+        st.image(image1)
+    st.write('地址：1111 S Figueroa St, Los Angeles, CA 90015美國, 觀眾席數：18,997人')
+    
 def Phoenix_Suns_map():  #太陽
     st.header('主場:足跡中心')
     Footprint_Center= folium.Map(location=[33.445833,-112.071389], zoom_start=16)
@@ -52,4 +69,21 @@ def Phoenix_Suns_map():  #太陽
     with col2:        
         image1 = Image.open('teams picture/Footprint_Center_Tickets.jpg')
         st.image(image1)
-    st.write('地址：US Airways Center, 201S 1st St, Phoenix, AZ 85004美國, 觀眾席數：18,422人')  
+    st.write('地址：US Airways Center, 201S 1st St, Phoenix, AZ 85004美國, 觀眾席數：18,422人')
+
+def Phoenix_Suns_map():  #國王
+    st.header('主場:金州第一中心')
+    Golden1_Center= folium.Map(location=[38.580361, -121.499611], zoom_start=16)
+        # add marker for Liberty Bell
+    tooltip = "金州第一中心"
+    folium.Marker([38.580361, -121.499611], popup="金州第一中心", tooltip=tooltip
+    ).add_to(Golden1_Center)
+    folium_static(Golden1_Center)
+    col1, col2 = st.columns(2)
+    with col1:
+        image = Image.open('teams picture/Golden1_Center.jpg')
+        st.image(image)        
+    with col2:        
+        image1 = Image.open('teams picture/Golden1_Center_Tickets.jpg')
+        st.image(image1)
+    st.write('地址：500 David J Stern Walk, Sacramento, CA 95814美國, 觀眾席數：17,500人')  
