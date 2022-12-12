6bb613ae4fe34bee6142f61a94dfef31496d29a3
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-12 21:21:36 +0800

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -18,18 +18,23 @@ if option=='Pacific':
   if teams=='Golden State Warriors':
     Pacific.Golden_State_Warriors()
     Pacific_map.Golden_State_Warriors_map()
+    Pacific_star.Golden_State_Warriors_star()
   if teams=='Los Angeles Clippers':
     Pacific.Los_Angeles_Clippers()
     Pacific_map.Los_Angeles_Clippers_map()
+    Pacific_star.Los_Angeles_Clippers_star()
   if teams=='Los Angeles Lakers':
     Pacific.Los_Angeles_Lakers()
     Pacific_map.Los_Angeles_Lakers_map()
+    Pacific_star.Los_Angeles_Lakers_star()
   if teams=='Phoenix Suns':
     Pacific.Phoenix_Suns()
     Pacific_map.Phoenix_Suns_map()
+    Pacific_star.Phoenix_Suns_star()
   if teams=='Sacramento Kings':
     Pacific.Sacramento_Kings()
     Pacific_map.Sacramento_Kings_map()
+    Pacific_star.Sacramento_Kings_star()
     
 if option=='Southwest':
   
@@ -37,15 +42,21 @@ if option=='Southwest':
   if teams=='Dallas Mavericks':
     Southwest.Dallas_Mavericks()
     Southwest_map.Dallas_Mavericks_map()
+    Southwest_star.Dallas_Mavericks_star()
   if teams=='Houston Rockets':
     Southwest.Houston_Rockets()
     Southwest_map.Houston_Rockets_map()
+    Southwest_star.Houston_Rockets_star()
   if teams=='Memphis Grizzlies':
     Southwest.Memphis_Grizzlies()
     Southwest_map.Memphis_Grizzlies_map()
+    Southwest_star.Memphis_Grizzlies_star()
   if teams=='New Orleans Pelicans':
     Southwest.New_Orleans_Pelicans()
     Southwest_map.New_Orleans_Pelicans_map()
+    Southwest_star.New_Orleans_Pelicans_star()
   if teams=='San Antonio Spurs':
     Southwest.San_Antonio_Spurs()
     Southwest_map.San_Antonio_Spurs_map()
+    Southwest_star.San_Antonio_Spurs_star()
+    
