e7c8f0f792d40a0c0ca594dd4ba6112f599891fd
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-13 15:26:27 +0800

    Update Pacific_allplayers.py

--- Pacific_allplayers.py
+++ Pacific_allplayers.py
@@ -1,7 +1,1 @@
-import streamlit as st  
-import pandas as pd
-import xlrd  
-import openpyxl
-def BostonCeltics_allplayers():
-  df = pd.read_excel("star/sportsref_download.xls") 
-  st.dataframe(df)
+
