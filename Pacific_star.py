17be7c2b5e640745e4b2a93b982291a6f7cdc150
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-14 09:43:10 +0800

    Update Pacific_star.py

--- Pacific_star.py
+++ Pacific_star.py
@@ -12,8 +12,8 @@ def Golden_State_Warriors_star():
   with col1:
     option=st.selectbox('選擇球星？',['Tim Hardaway', 'Klay thompson', 'Stephen Curry'])
     if option=='Tim Hardaway':
-      new_df = df[0:1]  
-      st.dataframe(new_df)
+      st.markdown(new_df = df[0:1])
+      st.markdown(st.dataframe(new_df))
     if option=='Klay thompson':
       new_df = df[1:2]
       st.dataframe(new_df)
