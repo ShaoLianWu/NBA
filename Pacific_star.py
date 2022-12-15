29626955fe6fedaf63e3aa7de9b4640ba4099e4e
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-15 09:06:02 +0800

    Update Pacific_star.py

--- Pacific_star.py
+++ Pacific_star.py
@@ -23,14 +23,6 @@ def Golden_State_Warriors_star():
         st.markdown("總得分")
       with col2:
         new_df = df[1:2]
-        st.markdown(print(new_df))
-        st.markdown("")
-        st.markdown("")
-        st.markdown("")
-        st.markdown("")
-        st.markdown("")
-        st.markdown("")
-        new_df = df[1:2]
         st.dataframe(new_df)
     if option=='Klay thompson':
       new_df = df[1:2]
