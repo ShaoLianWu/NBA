814cba6c7ebd0b1017d2a7cf4ad70f89e21e8eb8
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-15 09:03:04 +0800

    Update Pacific_star.py

--- Pacific_star.py
+++ Pacific_star.py
@@ -22,7 +22,8 @@ def Golden_State_Warriors_star():
         st.markdown("總助攻")
         st.markdown("總得分")
       with col2:
-        st.markdown(" "+str(df[1:2].iloc[0]['shots_on_goal']))
+        st.markdown(new_df = df[1:2],
+                    +str(df[1:2].iloc[0]['shots_on_goal']))
         st.markdown("")
         st.markdown("")
         st.markdown("")
