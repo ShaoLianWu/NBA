7ec5f044e3cf25ab42aaa19a1dd486e8ab024f85
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-15 09:36:35 +0800

    Update Pacific_star.py

--- Pacific_star.py
+++ Pacific_star.py
@@ -3,13 +3,6 @@ from PIL import Image
 import pandas as pd
 import xlrd
 import openpyxl
-import time
-
-my_bar = st.progress(0)
-
-for percent_complete in range(100):
-    time.sleep(0.1)
-    my_bar.progress(percent_complete + 1)
 
 df = pd.read_excel("star/Pacific_Southwest_star.xlsx",sheet_name="工作表1",usecols="A:H") 
 
