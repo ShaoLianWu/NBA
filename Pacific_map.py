a6e3c76c98b1c2ccbff9f03eec6d6f84226a53b1
ShaoLianWu <95857548+ShaoLianWu@users.noreply.github.com>
2022-12-19 20:53:01 +0800

    Update Pacific_map.py

--- Pacific_map.py
+++ Pacific_map.py
@@ -5,7 +5,7 @@ from streamlit_folium import folium_static
 
 def Golden_State_Warriors_map():  #勇士
     st.header('主場:大通銀行中心')
-    col1 ,col2 = st.columns((3,2))
+    col1 ,col2 = st.columns(2)
     with col1:
         Chase_Center= folium.Map(location=[37.768056, -122.3875], zoom_start=16)
             # add marker for Liberty Bell
